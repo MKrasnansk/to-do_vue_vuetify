2c148182180c12a81998aaee71bb4d92f1385734
MKrasnansk <miso.krasnansky@gmail.com>
2021-02-01 14:47:49 +0100

    first step

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,79 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+	<v-app id="inspire">
+		<v-navigation-drawer v-model="drawer" app>
+			<v-list-item>
+				<v-list-item-content>
+					<v-list-item-title class="title">
+						To do app
+					</v-list-item-title>
+					<v-list-item-subtitle>
+						Vuetify ToDo
+					</v-list-item-subtitle>
+				</v-list-item-content>
+			</v-list-item>
+
+			<v-divider></v-divider>
+
+			<v-list dense nav>
+				<v-list-item
+					v-for="item in items"
+					:key="item.title"
+					:to="item.to"
+					link
+				>
+					<v-list-item-icon>
+						<v-icon>{{ item.icon }}</v-icon>
+					</v-list-item-icon>
+
+					<v-list-item-content>
+						<v-list-item-title>{{
+							item.title
+						}}</v-list-item-title>
+					</v-list-item-content>
+				</v-list-item>
+			</v-list>
+		</v-navigation-drawer>
+
+		<v-app-bar
+			app
+			color="#6A76AB"
+			dark
+			prominent
+			src="https://picsum.photos/1920/1080?random"
+			fade-img-on-scroll
+		>
+			<template v-slot:img="{ props }">
+				<v-img
+					v-bind="props"
+					gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+				></v-img>
+			</template>
+
+			<v-app-bar-nav-icon
+				@click="drawer = !drawer"
+			></v-app-bar-nav-icon>
+
+			<v-app-bar-title>Michal Todo App</v-app-bar-title>
+		</v-app-bar>
+
+		<v-main>
+			<router-view></router-view>
+		</v-main>
+	</v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+	data: () => ({
+		drawer: null,
+		items: [
+			{
+				title: 'To do',
+				icon: 'mdi-format-list-checks',
+				to: '/'
+			},
+			{ title: 'About', icon: 'mdi-help-box', to: '/about' }
+		]
+	})
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
